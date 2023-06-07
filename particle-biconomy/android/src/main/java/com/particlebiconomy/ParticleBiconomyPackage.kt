da40e5f62f06a26d01130de972ecb69feca4ddc8
Houn <xuxihong@minijoy.work>
2023-06-07 20:32:00 +0800

    fix

--- particle-biconomy/android/src/main/java/com/particlebiconomy/ParticleBiconomyPackage.kt
+++ particle-biconomy/android/src/main/java/com/particlebiconomy/ParticleBiconomyPackage.kt
@@ -4,11 +4,12 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import com.particlebiconomy.ParticleAuthPlugin
 
 
 class ParticleBiconomyPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(ParticleBiconomyModule(reactContext))
+    return listOf(ParticleAuthPlugin(reactContext))
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {

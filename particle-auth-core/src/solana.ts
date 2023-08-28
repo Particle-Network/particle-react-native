0c5ef99cc78b9dbe4b1c98f1274cc89f4e235ad2
Houn <xuxihong@minijoy.work>
2023-08-28 10:57:40 +0800

    fix

--- particle-auth-core/src/solana.ts
+++ particle-auth-core/src/solana.ts
@@ -1,40 +1,48 @@
-import { ParticleAuthCorePlugin } from "react-native-particle-auth-core";
+import { ParticleAuthCorePlugin } from 'react-native-particle-auth-core';
 
-
-export async function getAddress() : Promise<string> {
-    return await ParticleAuthCorePlugin.solanaGetAddress();
+export async function getAddress(): Promise<string> {
+  return await ParticleAuthCorePlugin.solanaGetAddress();
 }
-    
 
 export async function signMessage(message: string): Promise<any> {
-    return new Promise((resolve) => {
-        ParticleAuthCorePlugin.solanaSignMessage(message, (result: string) => {
-            resolve(JSON.parse(result));
-        });
+  return new Promise((resolve) => {
+    ParticleAuthCorePlugin.solanaSignMessage(message, (result: string) => {
+      resolve(JSON.parse(result));
     });
+  });
 }
 
 export async function signTransaction(transaction: string): Promise<any> {
-    return new Promise((resolve) => {
-        ParticleAuthCorePlugin.solanaSignTransaction(transaction, (result: string) => {
-            resolve(JSON.parse(result));
-        });
-    });
+  return new Promise((resolve) => {
+    ParticleAuthCorePlugin.solanaSignTransaction(
+      transaction,
+      (result: string) => {
+        resolve(JSON.parse(result));
+      }
+    );
+  });
 }
 
-export async function signAllTransactions(transactions: string[]): Promise<any> {
-    const json = JSON.stringify(transactions);
-    return new Promise((resolve) => {
-        ParticleAuthCorePlugin.solanaSignAllTransactions(json, (result: string) => {
-            resolve(JSON.parse(result));
-        });
+export async function signAllTransactions(
+  transactions: string[]
+): Promise<any> {
+  const json = JSON.stringify(transactions);
+  return new Promise((resolve) => {
+    ParticleAuthCorePlugin.solanaSignAllTransactions(json, (result: string) => {
+      resolve(JSON.parse(result));
     });
+  });
 }
 
-export async function signAndSendTransaction(transaction: string): Promise<any> {
-    return new Promise((resolve) => {
-        ParticleAuthCorePlugin.solanaSignAndSendTransaction(transaction, (result: string) => {
-            resolve(JSON.parse(result));
-        });
-    });
+export async function signAndSendTransaction(
+  transaction: string
+): Promise<any> {
+  return new Promise((resolve) => {
+    ParticleAuthCorePlugin.solanaSignAndSendTransaction(
+      transaction,
+      (result: string) => {
+        resolve(JSON.parse(result));
+      }
+    );
+  });
 }

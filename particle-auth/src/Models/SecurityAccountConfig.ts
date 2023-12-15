4e73f7c0df6fb5b80aa795c05c90d31410bf7420
Zhichao Link <sunzhichao@minijoy.work>
2023-12-15 19:20:34 +0800

    update docs

--- particle-auth/src/Models/SecurityAccountConfig.ts
+++ particle-auth/src/Models/SecurityAccountConfig.ts
@@ -5,9 +5,9 @@ export class SecurityAccountConfig {
 
     /**
      *
-     * @param promptSettingWhenSign If show prompt when new acount sign, 0 no prompt, 1 first time show prompt, 2 every time show prompt, default value is 1
+     * @param promptSettingWhenSign If show prompt when new acount sign, 0 no prompt, 1 first time show prompt, 2 every time show prompt, 3 force show prompt, default value is 1
      *
-     * @param promptMasterPasswordSettingWhenLogin If show master password prompt when login, 0 no prompt, 1 first time show prompt, 2 every time show prompt, default value is 0
+     * @param promptMasterPasswordSettingWhenLogin If show master password prompt when login, 0 no prompt, 1 first time show prompt, 2 every time show prompt, 3 force show prompt, default value is 0
      */
     constructor(promptSettingWhenSign: number, promptMasterPasswordSettingWhenLogin: number) {
         this.promptSettingWhenSign = promptSettingWhenSign;

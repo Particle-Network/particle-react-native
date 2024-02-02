0dd16e74077b80530a31738d5e13e6a2183db3c1
jikang.wu <jikang.wu@kikitrade.com>
2024-02-02 18:02:08 +0800

    fix: update 30_000 to 30000

--- particle-connect/src/provider/connection.ts
+++ particle-connect/src/provider/connection.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: jikang.wu jikang.wu@kikitrade.com
- * @Date: 2024-02-02 17:59:00
- * @LastEditors: jikang.wu jikang.wu@kikitrade.com
- * @LastEditTime: 2024-02-02 18:00:28
- * @FilePath: /particle-react-native-fork/particle-connect/src/provider/connection.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import axios from 'axios';
 import type { ConnectionOptions, RequestArguments } from './types';
 
